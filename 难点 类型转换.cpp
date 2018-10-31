21ebedb5963aad1a52483969eda0790ca5c9e6eb
asdfghwunai <1729589198@qq.com>
2018-10-31 21:51:55 +0800

    Create 难点 类型转换.cpp

---
+++ 难点 类型转换.cpp
@@ -0,0 +1,11 @@
+/*
+1.不要混用带符号和无符号类型，啥结果你自己分析，这转那什么的
+2.俩对象进行运算，先各自提升，再决定谁转谁
+*/
+
+unsigned char c=-1;  //还能hold住
+signed char c2=256; //xxx, 超过表示范围，大小未定义
+
+string s,t="a value";   //有转换
+while(cin>>s)  //istream对象转换成bool
+
