2149b7770ffb347bc3337e195f06b4a0ae46b1b7
asdfghwunai <1729589198@qq.com>
2018-10-31 22:03:28 +0800

    Update 难点 类型转换.cpp

--- 难点 类型转换.cpp
+++ 难点 类型转换.cpp
@@ -8,4 +8,10 @@ signed char c2=256; //xxx, 超过表示范围，大小未定义
 
 string s,t="a value";   //有转换
 while(cin>>s)  //istream对象转换成bool
+  
+static_cast<>：不要用于包含底层const的，比如const int* 和const引用
+dynamic_cast<>
+const_cast<>: 只用于去除底层const属性，不用于类型转换
+reinterpret_cast<>： 不要用了，也不是大神啊
+
 
