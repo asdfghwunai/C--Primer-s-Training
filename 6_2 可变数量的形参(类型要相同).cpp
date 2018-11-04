dd35efe9416649e845225b5094898676046b0fb0
asdfghwunai <1729589198@qq.com>
2018-11-04 21:35:50 +0800

    Update 6_2 可变数量的形参(类型要相同).cpp

--- 6_2 可变数量的形参(类型要相同).cpp
+++ 6_2 可变数量的形参(类型要相同).cpp
@@ -1,4 +1,8 @@
 用到initializer_list<T> li; //里面的元素都是const类型
+初始化可以
+initializer_list<T> li;
+initializer_list<T> li({"",""});  
+initializer_list<T> li={};  //我看定义里没定义拷贝构造函数
 
 void error_msg(initializer_list<T> li)
 {
