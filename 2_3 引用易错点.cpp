4f049a3b842adb8edc71d196fd855a16e305161b
asdfghwunai <1729589198@qq.com>
2018-11-04 20:55:55 +0800

    Update 2_3 引用易错点.cpp

--- 2_3 引用易错点.cpp
+++ 2_3 引用易错点.cpp
@@ -2,5 +2,5 @@ int &refval4=10;   //错误， 初始化必须是对象
 double dval=3.14; 
 int &refval5=&dval; //错误，类型不匹配
 
-int &refs[10]=/* ? */;   //不存在引用的数组
+int &refs[10]=/* ? */;   //xxx,不存在引用的数组
 int (&arrRef)[10]=arr;   //数组的引用
