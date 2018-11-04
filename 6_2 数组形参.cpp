0104647b8027beb7bd7008c76b92132089f69bf5
asdfghwunai <1729589198@qq.com>
2018-11-04 21:00:42 +0800

    Update 6_2 数组形参.cpp

--- 6_2 数组形参.cpp
+++ 6_2 数组形参.cpp
@@ -6,4 +6,11 @@ void print(const int[10])  //xxx,三个等价的，算重定义，这是底层
 数组传参要传一个大小，不然不知道
 void print(const int ia[],size_t size)
 
+数组引用形参
+void print(int (&arr)[10])
+  
+传递多维数组
+void print(int (*matrix)[10],int rowSize);
+void print(int matrix[][10],int rowSize);
+
 
